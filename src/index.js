cf0a6142d80db000e52058f9a72c5e33ee1f28d3
Awambeng <awambengrodrick@gmail.com>
2024-02-05 10:25:50 +0100

    added: Basics of javascript

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// outputing messages in javascript
+console.log("Hello world")
+console.log("I love pizza")
+
+window.alert("This is an alert!!!")
+window.prompt("What is your name?")
+
+// This is a comment in javascript
+
+/*  Multiple lines 
+    
+    comment 
+    
+    in javascript */
+
+// Getting an HTML element by its id and assigning it a value
+document.getElementById("H1").textContent =  'Hello World';
+document.getElementById("P1").textContent = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa error eligendi et adipisci aliquid incidunt dolorum optio, quam ex dignissimos consectetur quia accusantium tenetur aperiam assumenda cumque natus. Libero, minima!";
